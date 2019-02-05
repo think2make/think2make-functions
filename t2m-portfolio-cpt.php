da8a684c0361b70e2f5f58bd3edbd65d2f2d3b62
Manuel Schmalstieg <git.code@ms-studio.net>
2019-02-05 17:49:10 +0100

    renommage des fonctions

--- t2m-portfolio-cpt.php
+++ t2m-portfolio-cpt.php
@@ -1,8 +1,8 @@
 <?php
 
-add_filter('register_post_type_args', 'porfolio_gutenberg', 10, 2);
+add_filter('register_post_type_args', 't2m_porfolio_gutenberg', 10, 2);
 
-function porfolio_gutenberg($args, $post_type){
+function t2m_porfolio_gutenberg($args, $post_type){
  
   if ($post_type == 'portfolio'){
     $args['show_in_rest'] = 'true';
